10c675198b4905a409484faf21e7a0e7f39e27ca
Pedro Henrique <Ph9084533@gmail.com>
2025-06-16 17:57:23 -0300

    Add files via upload
    Adicionando ao repositório, todos os arquivos com os script's JAVA do Grupo de Estudos. OBS: Ainda será realizado às atividades.

---
+++ Grupo-de-Estudos-POO-JAVA-IFCE/MAIO.java
@@ -0,0 +1,19 @@
+// Grupo de Estudos: POO Java
+// Discente: Pedro Henrique
+// Docente: Paulo Cesar
+
+// AT1 de Maio: Programa que realiza a adição de dois números em JAVA.
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Meu primeiro programa em Java!");
+        System.out.println("IFCE - Campus Tianguá");
+        
+        // Calculando a soma de dois números
+        int num1 = 5;
+        int num2 = 10;
+        int soma = num1 + num2;
+        
+        System.out.println("A soma de " + num1 + " e " + num2 + " é: " + soma);
+    }
+}
